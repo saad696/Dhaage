d7661a990dd8d25d1cf3aa600468f44c7a1e895c
Saad Shaikh <shksaad911@gmail.com>
2023-12-04 22:27:07 +0530

    created the basic layout and added authentication with onboarding process

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import TopBar from "@/components/shared/TopBar";
+import LeftBar from "@/components/shared/LeftBar";
+import RightBar from "@/components/shared/RightBar";
+import BottomBar from "@/components/shared/BottomBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dhaage",
+  description: "Dhaage | Share your thoughts.",
+  icons: {
+    icon: "favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <TopBar />
+          <main className="flex flex-row">
+            <LeftBar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightBar />
+          </main>
+          <BottomBar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
